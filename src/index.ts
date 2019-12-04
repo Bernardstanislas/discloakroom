4aa5aa06141162ab08a1137fcd2b0455704052ab
Stanislas Bernard <bernardstanislas@gmail.com>
2019-12-04 15:51:44 +0100

    :alembic: React to door opening

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,28 @@
 
 // TODO: Type correctly the lib
 const player = require("play-sound")({});
-const audio = player.play("sample.mp3");
+import rpio from "rpio";
+
+let audio = player.play("sample.mp3");
+
+const pause = () => audio.kill("SIGSTOP");
+
+const resume = () => audio.kill("SIGCONT");
+
+const LEFT_DOOR = 11;
+
+rpio.open(LEFT_DOOR, rpio.INPUT, rpio.PULL_UP);
+
+const onLeftDoorChange = (pin: number) => {
+  rpio.msleep(20);
+
+  const state = rpio.read(pin);
+
+  if (state) {
+    resume();
+  } else {
+    pause();
+  }
+};
+
+rpio.poll(LEFT_DOOR, onLeftDoorChange, rpio.POLL_BOTH);
