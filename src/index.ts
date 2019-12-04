bc171d1db492334d5ef93534244a047659b967ed
Stanislas Bernard <bernardstanislas@gmail.com>
2019-12-04 15:30:33 +0100

    :alembic: Try out audio library

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("Yolo");
+// Inspired by https://github.com/MattVador/node-red-contrib-play-sound/blob/master/node-red-contrib-play-sound.js
+
+// TODO: Type correctly the lib
+const player = require("play-sound")({});
+const audio = player.play("sample.mp3");
